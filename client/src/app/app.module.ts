fbd317c954c49424b3355577cc6c09717b3b3406
Anton Trofymchuk <a.trofimchuk@creatio.com>
2022-05-25 20:22:51 +0300

    ng-bootstrap with basic components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppHeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/nav.component';
+import { AppFooterComponent } from './footer/footer.component';
+import { AppNavItemComponent } from './nav-item/nav-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        AppHeaderComponent,
+        AppFooterComponent,
+        AppNavComponent,
+        AppNavItemComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, NgbModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ client/src/app/nav-item/nav-item.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nav-item',
+    templateUrl: './nav-item.component.html',
+    styleUrls: ['./nav-item.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppNavItemComponent {}
