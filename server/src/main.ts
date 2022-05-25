86adbb5c60b3e366bdf1eab6feb90e0c6d2e334d
Anton Trofymchuk <a.trofimchuk@creatio.com>
2022-05-25 18:13:15 +0300

    environment variables setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const APPLICATION_PORT = Number(process.env.APPLICATION_PORT) || 3000;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(APPLICATION_PORT, () =>
+        console.log(`Listening on port ${APPLICATION_PORT}`),
+    );
 }
 bootstrap();
