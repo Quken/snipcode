2ed545fdf7a7222e774ebed05fdace641b7a1109
Anton Trofymchuk <anton.trofymchu@ihsmarkit.com>
2022-05-31 19:26:35 +0300

    snippet preview

--- client/src/app/shared/components/snippets-list/snippet-list-item/snippet-list-item.component.ts
+++ client/src/app/shared/components/snippets-list/snippet-list-item/snippet-list-item.component.ts
@@ -1,12 +1,18 @@
 import {
+    AfterViewInit,
     ChangeDetectionStrategy,
+    ChangeDetectorRef,
     Component,
+    ElementRef,
     EventEmitter,
     Input,
     Output,
+    ViewChild,
 } from '@angular/core';
+import { AceService } from '@core/ace';
 import { Snippet } from '@core/snippets';
 import { GUID } from '@shared/models';
+import * as ace from 'ace-builds';
 
 @Component({
     selector: 'app-snippet-list-item',
@@ -14,12 +20,40 @@ import { GUID } from '@shared/models';
     styleUrls: ['./snippet-list-item.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SnippetListItemComponent {
+export class SnippetListItemComponent implements AfterViewInit {
+    @ViewChild('editor', { static: true })
+    public editorElementRef!: ElementRef<HTMLElement>;
+
+    private _aceEditor!: ace.Ace.Editor;
+    private readonly _lines: number = 10;
+    private readonly _maxLength: number = 130;
+
     @Output()
     public openSnippetEventEmitter: EventEmitter<GUID> = new EventEmitter();
 
     @Input()
     snippet!: Snippet;
+
+    constructor(
+        private readonly _aceService: AceService,
+        private readonly _cdr: ChangeDetectorRef
+    ) {}
+
+    public ngAfterViewInit(): void {
+        this._aceEditor = this._aceService.getAceEditor(
+            this.editorElementRef.nativeElement
+        );
+        this._aceEditor.session.setOption('useWorker', false);
+        this._aceEditor.setOptions({
+            minLines: this._lines,
+            maxLines: this._lines,
+            readOnly: true,
+        });
+        const shortenedSource = this.snippet.srcRaw.slice(0, this._maxLength);
+        this._aceEditor.session.setValue(shortenedSource);
+        this._aceEditor.session.setMode(`ace/mode/${this.snippet.language}`);
+        this._cdr.detectChanges();
+    }
 
     public onClick(): void {
         this.openSnippetEventEmitter.emit(this.snippet.id);
