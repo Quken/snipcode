de85be671d0db60a78d3b49e7a76e9d97513314e
Anton Trofymchuk <anton.trofymchu@ihsmarkit.com>
2022-06-03 18:14:36 +0300

    not-found page

--- client/src/app/shared/components/index.ts
+++ client/src/app/shared/components/index.ts
@@ -1,2 +1,3 @@
 export * from './carousel';
 export * from './snippets-list';
+export * from './not-found';
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselModule, SnippetsListModule } from './components';
+import {
+    CarouselModule,
+    NotFoundComponent,
+    SnippetsListModule,
+} from './components';
 import { SnippetModalComponent } from './components/snippet-modal/snippet-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -12,6 +16,6 @@ import { ReactiveFormsModule } from '@angular/forms';
         ReactiveFormsModule,
     ],
     exports: [CarouselModule, SnippetsListModule, ReactiveFormsModule],
-    declarations: [SnippetModalComponent],
+    declarations: [SnippetModalComponent, NotFoundComponent],
 })
 export class SharedModule {}
