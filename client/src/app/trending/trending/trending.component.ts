b3f20df2b39cd1da7fee7a3dca59e7151b2e7ea7
Anton Trofymchuk <anton.trofymchu@ihsmarkit.com>
2022-06-04 16:27:54 +0300

    likes developing

--- client/src/app/trending/trending/trending.component.ts
+++ client/src/app/trending/trending/trending.component.ts
@@ -1,7 +1,14 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import {
+    Component,
+    ChangeDetectionStrategy,
+    OnInit,
+    OnDestroy,
+} from '@angular/core';
 import { Snippet, SnippetsService } from '@core/snippets';
-import { map } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 import * as _ from 'lodash';
+import { GUID } from '@shared/models';
+import { User, UserService } from '@core/user';
 
 @Component({
     selector: 'app-trending',
@@ -9,7 +16,10 @@ import * as _ from 'lodash';
     styleUrls: ['./trending.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TrendingComponent implements OnInit {
+export class TrendingComponent implements OnInit, OnDestroy {
+    private _subscriptions: Subscription = new Subscription();
+    private _user!: User;
+
     public snippets$ = this._snippetsService.allSnippets$.pipe(
         map((snippets: Snippet[]) => {
             return [...snippets].sort(
@@ -18,9 +28,32 @@ export class TrendingComponent implements OnInit {
         })
     );
 
-    constructor(private readonly _snippetsService: SnippetsService) {}
+    constructor(
+        private readonly _snippetsService: SnippetsService,
+        private readonly _userService: UserService
+    ) {}
 
     public ngOnInit(): void {
         this._snippetsService.getAll();
+        this._subscriptions.add(
+            this._userService.user$.subscribe((user) => {
+                this._user = user;
+            })
+        );
+    }
+
+    public ngOnDestroy(): void {
+        this._subscriptions.unsubscribe();
+    }
+
+    public onLikeChange(snippetId: GUID, snippets: Snippet[]): void {
+        const snippet = snippets.find((snippet) => snippet.id === snippetId);
+        console.log(snippet);
+        console.log(this._user);
+        if (!this._user) {
+            return;
+        }
+        if (snippet?.createdBy.id !== this._user.id) {
+        }
     }
 }
