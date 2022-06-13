cbfd6e63bc3eb6a36d299faa656477a96777c1ef
antontrofymchuk@gmail.com <antontrofymchuk@gmail.com>
2022-06-13 11:18:59 +0300

    remove unused

--- server/src/user/models/user.ts
+++ server/src/user/models/user.ts
@@ -1,15 +1,5 @@
 import { GUID } from '@shared/models';
 
-// export interface UserConfig {
-//     id: GUID;
-//     email: string;
-//     name: string;
-//     surname: string;
-//     summary: string;
-//     age?: number;
-//     position?: string;
-// }
-
 export interface User {
     id: GUID;
     email: string;
@@ -18,22 +8,4 @@ export interface User {
     summary: string;
     age?: number;
     position?: string;
-
-    // constructor({
-    //     id,
-    //     name,
-    //     surname,
-    //     summary,
-    //     age,
-    //     position,
-    //     email,
-    // }: UserConfig) {
-    //     this.id = id;
-    //     this.email = email;
-    //     this.name = name;
-    //     this.surname = surname;
-    //     this.summary = summary;
-    //     this.age = age;
-    //     this.position = position;
-    // }
 }
